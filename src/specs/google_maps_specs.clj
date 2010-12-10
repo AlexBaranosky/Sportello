52d2aa2eb82ba6237cdcf319d4c60eba31fe29bb
AlexBaranosky <alexander.baranosky@gmail.com>
2010-12-09 21:50:08 -0500

    removed stubbing; it wasn't working right, and it now lives on the mocking branch

--- src/specs/google_maps_specs.clj
+++ src/specs/google_maps_specs.clj
@@ -5,23 +5,19 @@
   (:use clojure.walk)
   (:use collection-utils))
 
-;(defspec distance-in-miles-between-two-locations
-;  (dist-in-miles "NewYork,NY" "Boston,MA") => 219.061928254832)
+(defspec distance-in-miles-between-two-locations
+  (dist-in-miles "NewYork,NY" "Boston,MA") => 219.061928254832)
 
-;(defspec distances-in-miles-to-multiple-locations-from-origin
-;  (binding [dist-in-miles (fn [& args] 2.0)]
-;    (distances "Boston,MA" "Newport,RI" "LosAngeles,CA"))
-;  => [2.0 2.0])
+(defspec distances-in-miles-to-multiple-locations-from-origin
+  (distances "Boston,MA" "Newport,RI" "LosAngeles,CA")
+  => [2.0 2.0])
 
 (defspec distances-map-to-multiple-locations-from-origin
-  (stubbing [dist-in-miles 2.0]
-    (map-of-distances "Boston,MA" "Newport,RI" "LosAngeles,CA"))
+  (map-of-distances "Boston,MA" "Newport,RI" "LosAngeles,CA")
   => {"LosAngeles,CA" 2.0, "Newport,RI" 2.0})
 
 (defspec relative-values-of-locations-dependent-on-frequency-of-visits-per-year
-  (stubbing [dist-in-miles 365.0]
-    (println (dist-in-miles "goat" "farmer"))
-    (relative-distances "Boston,MA" "Newport,RI" 2 "LosAngeles,CA" 1))
+  (relative-distances "Boston,MA" "Newport,RI" 2 "LosAngeles,CA" 1)
   => {"LosAngeles,CA" 365.0, "Newport,RI" 730.0})
 
 (evaluate-specs)
