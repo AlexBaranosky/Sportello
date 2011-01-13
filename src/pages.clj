27dfaa95056d94218cd6f7c98bcc1d539220dc85
AlexBaranosky <alexander.baranosky@gmail.com>
2011-01-12 22:24:24 -0500

    removed extra remove-whitespace stuff from pages.cljcd proj/Sportello/! nice.

--- src/pages.clj
+++ src/pages.clj
@@ -7,13 +7,12 @@
 (defn home []
   (use-layout "home"))
 
-(defn list-distances [origin-w-whitespace addresses+freqs-w-whitespace]
-  (let [origin (remove-whitespace origin-w-whitespace)
-        [addresses freq-strings] (split-into-columns addresses+freqs-w-whitespace ":")
+(defn list-distances [origin addresses+freqs-w-whitespace]
+  (let [[addresses freq-strings] (split-into-columns addresses+freqs-w-whitespace ":")
         freqs (->> freq-strings (map remove-whitespace) (map #(Integer/parseInt %)))
         dists (apply map-of-distances origin addresses)
         total-dist-per-year (apply total-distance origin (interleave addresses freqs))]
-    (use-layout "list_distances" {"origin" origin-w-whitespace "distances" dists "totaldistance" total-dist-per-year})))
+    (use-layout "list_distances" {"origin" origin "distances" dists "totaldistance" total-dist-per-year})))
 
 (defn not-found-404 []
   (use-layout "not_found"))
